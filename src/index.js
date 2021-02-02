f24cd4a93b75ec247855f94a7df9e1df90ee20c8
McCotter36 <73186298+McCotter36@users.noreply.github.com>
2021-02-02 15:28:31 -0700

    updated files

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-spa';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.register();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,12 +17,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// serviceWorker.unregister();
 serviceWorker.register();
+
 atatus.config('6c4c9fc57e354ab382c9074b9efb480f').install();
-reportWebVitals();
+// reportWebVitals();
 
 // atatus.notify(new Error('Test Atatus Setup'));
