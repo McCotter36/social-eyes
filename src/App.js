1ba7d14b4caaf50d50db3d8d8dc91df854b3caca
McCotter36 <73186298+McCotter36@users.noreply.github.com>
2021-02-04 10:54:49 -0700

    added scatter chart
    added responsive scatter chart
    updated app name

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { getEvents, extractLocations } from './api';
 import { OfflineAlert } from './Alert';
-import './nprogress.css';
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer
+} from 'recharts';import './nprogress.css';
 
 class App extends Component {
   state = {
@@ -16,6 +18,16 @@ class App extends Component {
     alertText: "",
   }
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(' ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+  
   updateEvents = (location, eventCount) => {
     const { selectedLocation, numberOfEvents } = this.state;
     if (location) {
@@ -75,6 +87,22 @@ class App extends Component {
         <CitySearch className="CitySearch" locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents className="NumberOfEvents" numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
         </div>
+        <h4>Events in each city</h4>
+
+      <ResponsiveContainer height={400} >
+        <ScatterChart margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+          <CartesianGrid />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis
+            allowDecimals={false}
+            type="number"
+            dataKey="number"
+            name="number of events"
+          />
+          <Tooltip cursor={{ strokeDasharray: "3 3" }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
+      </ResponsiveContainer>
         <EventList className="EventList" events={this.state.events} />      
       </div>
     );
